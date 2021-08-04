8304dbe3163e8e47ba99007fc6d9ba5c0fd122e9
Airony <dxangusx@gmail.com>
2021-08-04 22:21:35 +0100

    Added API function to create a user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,16 +1,41 @@
 const asyncHandler = require('express-async-handler');
 const pool = require('../config/db.js');
+const bcrypt = require('bcrypt');
 
 //@desc    Fetch all users
 //@route    GET /api/users
 //access    public
 const getUsers = asyncHandler(async (req, res) => {
   try {
-    const { rows, fields } = await pool.query('SELECT * from "user";');
+    const { rows, fields } = await pool.query(
+      'SELECT id, full_name from "user";'
+    );
     res.json(rows);
   } catch (error) {
     console.log(error);
   }
 });
 
-module.exports = getUsers;
+//@desc    Add a new user
+//@route    POST /api/users
+//access    public
+const addUser = asyncHandler(async (req, res) => {
+  try {
+    let fullName = req.body.fullName;
+    let email = req.body.email;
+    let password = req.body.password;
+    let profilePicture = req.body.profilePicture;
+    let salt = await bcrypt.genSalt(10);
+    let hashedPassword = await bcrypt.hash(password, salt);
+    console.log(hashedPassword);
+    const { rows, fields } = await pool.query(
+      'INSERT INTO "user" (full_name, email, password, profile_picture) VALUES($1,$2,$3,$4);',
+      [fullName, email, hashedPassword, profilePicture]
+    );
+    res.json(rows);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+module.exports = { getUsers, addUser };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const getUsers = require('../controllers/userController.js');
+const { getUsers, addUser } = require('../controllers/userController.js');
 const router = express.Router();
 
 router.route('/').get(getUsers);
+router.route('/').post(addUser);
 
 module.exports = router;
