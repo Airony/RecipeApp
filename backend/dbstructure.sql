98f4b9dced9c47a597f007d7af29f9fab5ea20ce
Airony <dxangusx@gmail.com>
2021-08-07 21:35:00 +0100

    Changed the database so if users are deleted their recipes and comments will be deleted aswell

--- backend/dbstructure.sql
+++ backend/dbstructure.sql
@@ -9,7 +9,7 @@ CREATE TABLE "user"(
 
 CREATE TABLE "recipe"(
     recipeId SERIAL PRIMARY KEY NOT NULL,
-    userId INT NOT NULL references "user"(userId),
+    userId INT NOT NULL references "user"(userId) ON DELETE CASCADE,
     title VARCHAR(25) NOT NULL,
     avg_rating SMALLINT NOT NULL,
     description VARCHAR(500) NOT NULL,
@@ -24,12 +24,21 @@ CREATE TABLE "recipe"(
 
 CREATE TABLE "comment"(
     commentId SERIAL PRIMARY KEY NOT NULL,
-    userId INT NOT NULL references "user"(userId),
-    recipeId INT NOT NULL references "recipe"(recipeId),
+    userId INT NOT NULL references "user"(userId) ON DELETE CASCADE,
+    recipeId INT NOT NULL references "recipe"(recipeId) ON DELETE CASCADE,
     content TEXT NOT NULL,
     points INT NOT NULL DEFAULT 0,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
+
+CREATE TABLE "admin"(
+    userId INT NOT NULL references "user"(userId) ON DELETE CASCADE
+);
+
+CREATE TABLE "author"(
+    userId INT NOT NULL references "user"(userId) ON DELETE CASCADE
+);
+
 
 CREATE TABLE "session" (
   "sid" varchar NOT NULL COLLATE "default",
@@ -41,3 +50,9 @@ WITH (OIDS=FALSE);
 ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
 
 CREATE INDEX "IDX_session_expire" ON "session" ("expire");
+
+
+
+
+
+
