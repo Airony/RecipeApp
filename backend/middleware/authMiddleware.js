4591dda6f9633d231199f601dd9044a67ce4a985
Airony <dxangusx@gmail.com>
2021-08-10 14:47:15 +0100

    Added middleware to only allow adminstrator users to access certain endpoints

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -25,4 +25,20 @@ const authorOnly = asyncHandler(async (req, res, next) => {
   next();
 });
 
-module.exports = { authOnly, authorOnly };
+const adminOnly = asyncHandler(async (req, res, next) => {
+  const userId = req.session.userId;
+
+  const { rows } = await pool.query(
+    `SELECT * FROM "admin" WHERE userId = $1;`,
+    [userId]
+  );
+
+  if (rows.length == 0) {
+    res.status(403);
+    throw new Error("Only admins can do that.");
+  }
+
+  next();
+});
+
+module.exports = { authOnly, authorOnly, adminOnly };
